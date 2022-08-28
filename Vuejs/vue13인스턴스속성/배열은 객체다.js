511e3f120fd6e46c45e3671e2ab1a572368f674e
zzangu42 <jjangu02@naver.com>
2022-08-28 13:55:43 +0900

    22-08-21

--- Vuejs/vue13인스턴스속성/배열은 객체다.js
+++ Vuejs/vue13인스턴스속성/배열은 객체다.js
@@ -1,17 +1,26 @@
-var arr = [];
+debugger;
+var arr = []; //배열 숫자형
 arr[0] = 1;
 arr[1] = 2;
 console.log(arr);
 console.log(arr[0]);
 
-var arr = [];
+var arr = []; //배열 문자열
 arr['0'] = 1;
 arr['1'] = 2;
 console.log(arr);
 console.log(arr['0']);
 
-var arr = {};
+var arr = {}; //객체
 arr['0'] = 1;
 arr['1'] = 2;
 console.log(arr);
 console.log(arr['0']);
+
+for (const key in object) {
+    // v-for="(val, i) in arr"
+    // in 다음에 오는 애(변수) 중요 arr(변수)
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+    }
+}
