1dee665fd8f35b491fdd04978e7bd9ac61572bb8
zzangu42 <jjangu02@naver.com>
2022-09-04 18:06:23 +0900

    22-09-04

--- Vuejs/vue30컴포넌트실습/comprj03/src/components/Vue3403Comp.vue
+++ Vuejs/vue30컴포넌트실습/comprj03/src/components/Vue3403Comp.vue
@@ -1,36 +1,31 @@
 <template>
-    <div id="app">
-        자식 컴포넌트에서 받은 값: {{ mesg }}
-        <!--자식에서 받은 건 나를 바꿔야함-->
-        <child-component v-on:showLog="printText"></child-component>
+    <div>
+        <button v-on:click="showLog">show</button>
     </div>
 </template>
 
 <script>
-import Vue3403Comp from '../components/Vue3403Comp.vue';
 export default {
     /* pdtmc^2w */
     props: [],
     data() {
         /* 컴포넌트 안에서 사용되는 변수 등록. 개별 변수 */
-        return {
-            mesg: '부모에서 설정한 값'
-        };
+        return {};
     },
     //template: ``,
     methods: {
         /* 이벤트 핸들러 등록 + 일반 함수 */
-        printText(param1, param2) {
-            //console.log(arguments); //arguments 한번에 받을수있다
-            console.log(param1, param2);
+        showLog(e) {
+            console.log(e.target);
+            //showLog 라는 사용자 이벤트를 발생시키고
+            //"abc"와 123을 값으로 전달하시오
             debugger;
-            this.$data.mesg = `자식에서 받은 값: param1=${param1}, parma2=${param2}`;
+            this.$emit('showLog', 'abc', 123);
         }
     },
     components: {
         /* 전역 컴포넌트는 등록하지 않는다. */
         /* 지역 컴포넌트나 파일 컴포넌트이면 등록해야 한다 . 예시) "태그명" : 컴포넌트명 */
-        'child-component': Vue3403Comp
     },
     computed: {
         /* 자동처리 + 동기식. 메서드로 작성. return 필수. data 와 공존 불가 */
